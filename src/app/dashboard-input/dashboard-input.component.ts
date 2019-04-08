7e4c7753a69cba9cb29cb32d9ba934f3d42aebcc
ConQueRoR- <chikoo1998@gmail.com>
2019-04-08 14:58:36 +0530

    img upload try

--- src/app/dashboard-input/dashboard-input.component.ts
+++ src/app/dashboard-input/dashboard-input.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { weaponClass } from '../weaponClass';
 import { WeaponServiceService } from '../weapon-service.service';
 import { ActivatedRoute,Router } from '@angular/router';
+import {  FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
+
 @Component({
   selector: 'app-dashboard-input',
   templateUrl: './dashboard-input.component.html',
@@ -14,18 +16,24 @@ weapon_damage:string;
   weapon_FireRate:String;
   weapon_recoil:String;
   arr:weaponClass[];
+  selectedFile: File;
+  public uploader: FileUploader = new FileUploader({url: URL, itemAlias: 'photo'});
   constructor(private ws:WeaponServiceService,private route: ActivatedRoute,
     private router: Router,) { }
 
   ngOnInit() {
     this.ws.getdata().subscribe((data: weaponClass[]) => {this.arr = data;});
   }
-
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0]
+  }
   add_data(){
     this.ws
-    .add_data(this.weapon_name,this.weapon_damage,this.weapon_recoil,this.weapon_FireRate,this.weapon_mobility);
+    .add_data(this.weapon_name,this.weapon_damage,this.weapon_recoil,this.weapon_FireRate,this.weapon_mobility,this.selectedFile);
+    
     
   }
+  
   deleteData(id) {
     this.ws.deleteData(id).subscribe(res => {
       console.log('Deleted');
